4c1774f83629d22dde95fa796cdd465c3b8915f6
Michael Ababio <michaelkwasi@gmail.com>
2017-11-02 23:52:12 -0400

    inital commit

---
+++ Twilio_Name/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='twilio_name',
+      version='1.0',
+      description='provide phone number and get name assoicated with it.',
+      author='Michael Ababio',
+      author_email='michaelkwasi@gmail.com',
+      install_requires=['twilio'],
+      python_requires=">=3.0",
+      entry_points = {'console_scripts': ['twil_num=twilio_name.twilio_helper:main']},
+      packages=['twilio_name'],
+     )
