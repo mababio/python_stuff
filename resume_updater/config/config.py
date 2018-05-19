bb70b6e8818cdde77572582c0f58a811a6ffb68a
Michael Ababio <michaelkwasi@gmail.com>
2018-05-19 13:54:31 -0400

    change sms implementation code to reflect aws sns

--- resume_updater/config/config.py
+++ resume_updater/config/config.py
@@ -9,3 +9,4 @@ config.read(config_path + '/config.ini')
 
 aws_access_key_id = config['TWILIO']['aws_access_key_id'].strip("''")
 aws_secret_access_key = config['TWILIO']['aws_secret_access_key'].strip("'")
+phone_to = config['TWILIO']['phone_to'].strip("'")
--- resume_updater/notify/noti.py
+++ resume_updater/notify/noti.py
@@ -1,17 +1,26 @@
 # Download the twilio-python library from twilio.com/docs/libraries/python
 from twilio.rest import Client
 import os
+import boto3
 import sys
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__))+'/..')
 from config import config
 
+# Create an SNS client
+client = boto3.client(
+    "sns",
+    aws_access_key_id=config.aws_access_key_id,
+    aws_secret_access_key=config.aws_secret_access_key,
+    region_name="us-east-1"
+)
 
 def sms(message):
-    client = Client(config.account_sid, config.auth_token)
-    client.api.account.messages.create(
-        to= config.phone_num_to,
-        from_=config.phone_num_from,
-        body=message)
+    # Send your sms message.
+    client.publish(
+        PhoneNumber=config.phone_to,
+        Message=message
+    )
+
 
 def email():
     pass
