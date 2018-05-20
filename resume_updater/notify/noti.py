555c2e48f6481342b58fd143e3cbae017152e539
Michael Ababio <michaelkwasi@gmail.com>
2018-05-19 22:13:32 -0400

    clean up resume config

--- resume_updater/notify/noti.py
+++ resume_updater/notify/noti.py
@@ -6,15 +6,22 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__))+'/..')
 from config import config
 
-# Create an SNS client
-client = boto3.client(
-    "sns",
-    aws_access_key_id=config.aws_access_key_id,
-    aws_secret_access_key=config.aws_secret_access_key,
-    region_name="us-east-1"
-)
+key_id= config.aws_access_key_id
+key= config.aws_secret_access_key
+phone_num = config.phone_to
+
+def create_client():
+    # Create an SNS client
+    client = boto3.client(
+        "sns",
+        aws_access_key_id=config.aws_access_key_id,
+        aws_secret_access_key=config.aws_secret_access_key,
+        region_name="us-east-1"
+    )
+    return client
 
 def sms(message):
+    client = create_client()
     # Send your sms message.
     client.publish(
         PhoneNumber=config.phone_to,
